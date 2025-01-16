fa74879e5cb91d723c23b55470f3bff835f2ec6b
lzy <1016984801@qq.com>
2025-01-16 18:42:04 +0800

    feat: impl DWTMLEAD

--- ts_benchmark/baselines/self_impl/__init__.py
+++ ts_benchmark/baselines/self_impl/__init__.py
@@ -9,7 +9,8 @@ __all__ = [
     "TranAD",
     "MatrixProfile",
     "LeftSTAMPi",
-    "KMeans"
+    "KMeans",
+    "DWT_MLEAD",
 ]
 
 
@@ -25,4 +26,5 @@ from ts_benchmark.baselines.self_impl.MatrixProfile.MatrixProfile import MatrixP
 from ts_benchmark.baselines.self_impl.TranAD.TranAD import TranAD
 from ts_benchmark.baselines.self_impl.LeftSTAMPi.LeftSTAMPi import LeftSTAMPi
 from ts_benchmark.baselines.self_impl.KMeans.KMeans import KMeans
+from ts_benchmark.baselines.self_impl.DWT_MLEAD.DWTMLEAD import DWT_MLEAD
 
